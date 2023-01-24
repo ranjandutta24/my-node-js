884745301533bf6faa34c4e937a46fba7aa560ba
Ranjan Dutta <connecttoranjan@gmail.com>
2023-01-24 12:46:58 +0530

    git init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/validation")();
+
+require("./startup/prod")(app);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
